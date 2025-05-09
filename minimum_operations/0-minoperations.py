df1aaa3e1269056964bcdb570a9a960945793d69
isamuella <s.ineza@alustudent.com>
2025-05-09 11:29:57 +0000

    ex0

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+def minOperations(n):
+    if n <= 1:
+        return 0
+
+    operations = 0
+    i = 2
+
+    while n > 1:
+        while n % i == 0:
+            operations += i
+            n = n // i
+        i += 1
+
+    return operations
+
